962fae92215d43860ec6ddc6c8ba44190acdba55
André Rocha <106490437+deorocha@users.noreply.github.com>
2025-10-22 01:51:47 -0300

    Refactor file path handling for project structure

--- Topicos-BD-2/programas/Fluxograma Diagrama do processo.py
+++ Topicos-BD-2/programas/Fluxograma Diagrama do processo.py
@@ -1,16 +1,16 @@
 import streamlit as st
-# from pathlib import Path
+from pathlib import Path
 
 # Obtém o diretório raiz do projeto (onde está o app.py)
 # Este script está em uma subpasta, então precisamos subir um nível
-#current_dir = Path(__file__).parent if "__file__" in locals() else Path.cwd()
-#project_root = current_dir.parent  # Sobe um nível para a pasta raiz
+current_dir = Path(__file__).parent if "__file__" in locals() else Path.cwd()
+project_root = current_dir.parent  # Sobe um nível para a pasta raiz
 
 # Constrói caminhos absolutos para os arquivos na pasta raiz
 #css_path = project_root / "styles" / "styles.css"
 #fluxograma_image_path = project_root / "images" / "fluxograma.png"
-css_path = "../styles/styles.css"
-fluxograma_image_path = "../images/fluxograma.png"
+css_path = project_root / "styles" / "styles.css"
+fluxograma_image_path = project_root / "images" / "fluxograma.png"
 
 def load_css(css_path):
     try:
