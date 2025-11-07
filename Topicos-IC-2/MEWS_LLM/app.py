2dd7e91dbfa65dffe4efaa3f16df40350e64e835
André Rocha <106490437+deorocha@users.noreply.github.com>
2025-11-07 01:49:45 -0300

    Update app.py

--- Topicos-IC-2/MEWS_LLM/app.py
+++ Topicos-IC-2/MEWS_LLM/app.py
@@ -47,8 +47,7 @@ def init_chatbot():
     
     if models_dir.exists():
         files = list(models_dir.glob("*"))
-        arq = [f.name for f in files]
-        # st.sidebar.write(f"Arquivos em models: {[f.name for f in files]}")
+        st.sidebar.write(f"Arquivos em models: {[f.name for f in files]}")
     
     # Tenta carregar o modelo
     model_loaded = chatbot.load_model()
@@ -232,6 +231,7 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
 
 
