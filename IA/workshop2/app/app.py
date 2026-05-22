de9b5aacce3b3ff694183d9bcee169a2d7e6702c
André Rocha <106490437+deorocha@users.noreply.github.com>
2026-05-21 22:14:08 -0300

    Update app.py

--- IA/workshop2/app/app.py
+++ IA/workshop2/app/app.py
@@ -82,13 +82,15 @@ if uploaded_file is not None:
         top_class = list(sorted_probs.keys())[0]
         top_score = list(sorted_probs.values())[0]
 
-        st.success(f"**Diagnóstico Provável:** {top_class}")
-        # st.metric(label="Confiança", value=f"{top_score:.2f}%")
-        st.info(f"Confiança: {top_score:.2f}%")
+        col3, col4 = st.columns([1, 1])
+        with col3:
+            st.success(f"**Diagnóstico Provável:** {top_class}")
+        with col4:
+            st.info(f"Confiança: {top_score:.2f}%")
 
-        # Gráfico de barras
-        df_probs = pd.DataFrame(list(sorted_probs.items()), columns=['Doença', 'Probabilidade (%)'])
-        st.bar_chart(df_probs.set_index('Doença'))
+            # Gráfico de barras
+            df_probs = pd.DataFrame(list(sorted_probs.items()), columns=['Doença', 'Probabilidade (%)'])
+            st.bar_chart(df_probs.set_index('Doença'))
 
     # Tabela detalhada
     with st.expander("Ver todas as probabilidades"):
